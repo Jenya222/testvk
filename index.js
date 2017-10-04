f070b3cca1a3e746b36c06b0b95149161cdf33ab
Евгений  Е. Беспалов <ee.bespalov@devex.pro>
2017-10-04 20:28:23 +0300

    v1.1

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var client_id = '6201315';
-var scope = 'offline,messages';
+var scope = 'offline,messages,friends';
 var href = "https://oauth.vk.com/authorize?client_id="+client_id+"&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope="+scope+"&response_type=token&v=5.37";
-var accessToken =
-ReactDOM.render(
+var accessToken;
+var hrefFave;
+
+    ReactDOM.render(
     <div>
         <h1 id="title">
             Hello React
         </h1>
-        <a href={href}>Push the button</a>
+        <a href={href}>получить токен</a>
+        <input className='fieldAccessToken'/>
+        <a href='' className="buttonGetPhotos">получить залайканные фотки</a>
     </div>,    // элемент, который мы хотим создать
-    document.getElementById("container")    // где мы этот элемент хотим создать
-)
+    document.getElementById("container")  // где мы этот элемент хотим создать
+);
 
-
+document.querySelector(".buttonGetPhotos").addEventListener("click", function () {
+    accessToken = document.querySelector("input");
+    accessToken = accessToken.value;
+    hrefFave = "https://api.vk.com/method/fave.getPhotos?&access_token="+accessToken+"&v=5.68";
+    this.href = hrefFave;
+});
 
 
